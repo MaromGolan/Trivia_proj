c762dc9d2d3e26c3a5aa211245275f17ffe5f188
arik slavsky <arik16663@gmail.com>
2023-05-22 14:47:27 +0000

    fixed

--- json.hpp
+++ json.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <vector>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+
+namespace JsonBase
+{
+	typedef std::vector<char> Buffer;
+
+	const std::string CODE = "";
+
+	const int BYTE = 8;
+	const int DATA_LENGTH = 32;
+
+}
