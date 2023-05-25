b6ebb3cdb84e47f75969c7b580f7e4befbcf8b97
arik slavsky <arik16663@gmail.com>
2023-05-25 23:29:28 +0000

    Updated.

--- Structs.h
+++ Structs.h
@@ -9,7 +9,6 @@ struct LoginRequest
 };
 
 
-
 struct SignupRequest
 {
 	std::string _username;
@@ -19,3 +18,98 @@ struct SignupRequest
 	std::string	_date;
 
 };
+
+
+
+
+
+struct GameRoomData
+{
+	std::string _status;
+	std::string _admin;
+	std::string _name;
+
+	int _maxPlayerCount;
+	int _questionCount;
+	int _questionTime;
+};
+
+
+
+
+
+struct GoodResponse
+{
+	std::string _ok = "ok";
+};
+
+
+struct BadResponse
+{
+	std::string _error;
+};
+
+
+
+
+
+struct LoginRequest
+{
+	std::string _username;
+	std::string _password;
+};
+
+
+struct SignupRequest
+{
+	std::string _username;
+	std::string _password;
+	std::string _email;
+};
+
+
+
+
+
+struct GetRoomsResponse
+{
+	std::unordered_map<std::string, Room> _rooms;
+};
+
+
+struct GetPlayersInRoomResponse
+{
+	std::set<std::string> _players;
+};
+
+
+
+
+
+struct StartRoom
+{
+	std::string _roomName;
+};
+
+
+struct LeaveRoom
+{
+	std::string _use;
+	std::string _roomName;
+};
+
+
+struct CloseRoom
+{
+	std::string _roomName;
+};
+
+
+struct RoomStatistics
+{
+	std::string _roomName;
+};
+
+
+
+
