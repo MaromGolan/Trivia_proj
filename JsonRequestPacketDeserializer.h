a95215ccf39164a51403459cf784b24c4d9cc132
arik slavsky <arik16663@gmail.com>
2023-05-28 00:23:13 +0000

    Implementing the singleton bonus.

---
+++ JsonRequestPacketDeserializer.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <vector>
+
+#include "MenuRequest.h"
+#include "Structs.h"
+
+#include "jsonBase.h"
+
+using namespace JsonBase;
+
+
+class JsonRequestPacketDeserializer
+{
+
+public:
+
+
+	//methods;
+	static LoginRequest deserializeLoginRequest(const Buffer& buffer);
+	static SignupRequest deserializeSignupRequest(const Buffer& buffer);
+
+
+
+	//FUNCTION DECLARATIONS PREPARED FOR 2.0.0:
+	static LeaveRoom deserializeLeaveRoomReq(const Buffer& buffer);
+	static GetPlayersInRoom deserializeGetPlayersRequest(const Buffer& buffer);
+	static CreateRoom deserializeCreateRoomRequest(const Buffer& buffer);
+	static JoinRoomRequest deserializeJoinRoomRequest(const Buffer& buffer);
+
+
+
+	//the singleton bonus;
+
+	JsonRequestPacketDeserializer(JsonRequestPacketDeserializer const&) = delete;
+
+	void operator=(JsonRequestPacketDeserializer const&) = delete;
+
+	static JsonRequestPacketDeserializer& getInstance()
+	{
+		static JsonRequestPacketDeserializer entity;
+
+		return entity;
+	}
+
+
+private:
+
+	//constructor;
+	JsonRequestPacketDeserializer() = default;
+
+	//this is similar to "toString" in cs;
+	static void toJson(const Buffer& buffer, json& jsonContainer);
+
+};
+
+
+
+
